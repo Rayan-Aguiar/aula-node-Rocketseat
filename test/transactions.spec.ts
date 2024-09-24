9d0953bc9aaaa4d675986b97db15d44a1a2ac5b5
rayan-aguiar <rayansiqueira@gmail.com>
2024-09-24 16:39:11 -0300

    fim aula F050 - Categorizando os testes

---
+++ test/transactions.spec.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { test, beforeAll, afterAll, describe } from 'vitest'
+import { app } from '../src/app'
+import request from 'supertest'
+
+
+describe('Transaction routes', () =>{
+  beforeAll ( async () =>{
+    await app.ready()
+  })
+  
+  afterAll ( async () =>{
+      await app.close()
+  })
+  test('user can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+})
